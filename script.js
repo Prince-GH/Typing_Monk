2361cef33f312c5435759c043f034d0396553e78
Prince <150125481+Prince-GH@users.noreply.github.com>
2024-04-14 20:19:14 +0530

    Update

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
- let TotalTime = document.getElementById("TotalTime");
+  const form = document.querySelector('form');
+    const data = document.getElementById('data');
+    const Restart = document.querySelector('.reset');
+    const a_type = document.getElementById('a_type');
+    const a_back = document.getElementById('a_back');
+    const a_error = document.getElementById('a_error');
+    const a_done = document.getElementById('a_done');
     let TotalWords = document.getElementById("TotalWords");
-    let speed = document.getElementById("speed");
-    let error = document.getElementById("error");
     const keydata = document.getElementById("keydata");
     const Console = document.getElementById("console");
+    let error = document.getElementById("error");
     let text;
     let i=0;
-    (function StartTyping(){
-        keydata.innerHTML=prompt('Insert your text: ');
-        text = keydata.innerHTML.split("");
+    
+    Restart.addEventListener('click',(e)=>{
+        a_type.currentTime=0;
+        a_type.play();
+        form.style.display='flex';
+        Restart.innerHTML='Restart';
+    });
+    
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        keydata.innerHTML=data.value;
+        text=keydata.innerHTML.split("");
+        data.value='';
+        a_type.play();
+        form.style.display='none';
         Start();
-    })();
+    });
 
 function Start(){
     window.addEventListener("keydown",function (e){
        error.innerHTML="";
        let char = String(text[i]);
        keyCheck(char,e.key);
-
        console.log(char);
        console.log(i);
 
@@ -29,9 +45,10 @@ function Start(){
                 if(text.length==i){
                     end();
                 }
+                a_type.currentTime=0;
+                a_type.play();
             }
             Console.style.color='white';
-            
         }else{
             if(e.key==='Backspace'){
                     i--;
@@ -40,8 +57,12 @@ function Start(){
                     for(let j=0;j<TEXT.length-1;j++){
                         Console.innerHTML+=TEXT[j];
                     }
+                    a_back.currentTime=0;
+                    a_back.play();
                 }else{
                     error.innerHTML=`💡:${text[i]}`;
+                    a_error.currentTime=0;
+                    a_error.play();
                 }
         } 
 
@@ -58,9 +79,20 @@ function Start(){
     }
   
 function end(){
+    i=0;
     keydata.innerHTML='';
     Console.innerHTML='';
     TotalWords.innerHTML='';
     keydata.style.color='white';
     keydata.innerHTML='Congratulations🏆';
+    a_done.currentTime=0;
+    a_done.play();
+    
+    window.addEventListener('keydown',(e)=>{
+        a_back.pause();
+        a_error.pause();
+        a_type.pause();
+        a_done.pause();
+    })
+    setInterval(()=>{window.location.reload();},10000)
 }
